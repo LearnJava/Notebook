fdd875552f7b17575902305764665daf02cd0f08
LearnJava <java20012@gmail.com>
2021-07-01 21:11:51 +0300

    lesson10 firestore

---
+++ app/src/main/java/ru/konstantin/notebook/repository/NoteFirestoreRepository.java
@@ -0,0 +1,12 @@
+package ru.konstantin.notebook.repository;
+
+import java.util.List;
+
+import ru.konstantin.notebook.entity.Note;
+
+public class NoteFirestoreRepository implements NoteRepository {
+    @Override
+    public List<Note> getNotes() {
+        return null;
+    }
+}
